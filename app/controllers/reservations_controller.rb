fd8e233205cb332634e22b1200ef2b00341e0640
Your Name <arssonist@yahoo.com>
2016-09-05 12:03:48 -0400

    Working make reservation form, but rediects not working. restaurant creation also working

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,10 +9,30 @@ class ReservationsController < ApplicationController
 
   def show
     @reservation = Reservation.find(params[:id])
-    ensure_restaurant_match
+    if @reservation.save
+      redirect_to [@restaurant,@reservation]
+    else
+      redirect_to new_restaurant_reservation_path
   end
-
+end
   def new
     @reservation = Reservation.new
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.restaurant = @restaurant
+
+    if @reservation.save
+      redirect_to [@restaurant,@reservation]
+    else
+      redirect_to new_restaurant_reservation_path
+    end
+
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:number_of_guests, :date)
+  end
+
 end
